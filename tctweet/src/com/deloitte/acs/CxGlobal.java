e863e26b7452561246fc414fec557976f2667c51
ornatwork <ornsmail@gmail.com>
2011-06-05 11:22:03 -0700

    updated to reflect what needs to be changed

--- tctweet/src/com/deloitte/acs/CxGlobal.java
+++ tctweet/src/com/deloitte/acs/CxGlobal.java
@@ -4,15 +4,15 @@ public class CxGlobal
 {
 	// User
 	public static String TwitterName = "acsbuild";
-	public static String TwitterPass = "dev4acs";
+	public static String TwitterPass = "yourPass";
 	
 	// Consumer 
-	public static String CONSUMER_KEY = "DjQincgDEznSSS8zAEHQOg";
-	public static String CONSUMER_SECRET = "WuE6PFVszYE5M1y58BhlppCaRVzgUAqJnD060HtjIO4";
+	public static String CONSUMER_KEY = "YourKey";
+	public static String CONSUMER_SECRET = "YourSecret";
 	
 	// Access token 
-	public static String ACCESS_TOKEN_KEY = "44457853-Q8QiSdAE1NZZMtGruaceDhagI9eiwddtH8DSlGXEt";
-	public static String ACCESS_TOKEN_SECRET = "Vca8WpovYUGkchWe4U5BBz3lPCgI7IOa5IL8FR8FnA";
+	public static String ACCESS_TOKEN_KEY = "YourAccessToken";
+	public static String ACCESS_TOKEN_SECRET = "YourSecretToken";
 	
 
 }
